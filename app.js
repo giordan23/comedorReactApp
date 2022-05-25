976c308c9253731801e7922a24dcfb69bcf89ec6
unknown <giordan.23w@gmail.com>
2022-05-25 17:39:32 -0500

    falta Mostrar pedidos disponibles

---
+++ app.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { AppRouter } from "./src/routers/AppRouter";
+
+export const App = () => {
+	return (
+		<AppRouter></AppRouter>
+	);
+};
--- src/components/pedidos/PedidosScreen.js
+++ src/components/pedidos/PedidosScreen.js
@@ -8,13 +8,12 @@ export const PedidosScreen = () => {
 
 	const { registrarPedidoInput } = values;
 
-
 	const handleSearch = (e) => {
 		e.preventDefault();
 		console.log(registrarPedidoInput);
 		if (registrarPedidoInput.trim().length === 8 || registrarPedidoInput.trim().length === 10) {
 			//llama a peticion
-			postPedido(url,{id_estudiante: registrarPedidoInput, turno:'D'})
+			postPedido(url,{id_estudiante: registrarPedidoInput})
 
 			console.log('Pedido enviado ' + registrarPedidoInput);
 			reset()
@@ -26,12 +25,12 @@ export const PedidosScreen = () => {
 		return false
 	};
 
-
-
 	const url = 'http://127.0.0.1:8080/api/pedidos'
 
+	
 	const postPedido = async (url, data = {}) => {
 		// Opciones por defecto estan marcadas con un *
+
 		const response = await fetch(url, {
 			method: 'POST', // *GET, POST, PUT, DELETE, etc.
 			mode: 'cors', // no-cors, *cors, same-origin
@@ -45,11 +44,12 @@ export const PedidosScreen = () => {
 			referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
 			body: JSON.stringify(data) // body data type must match "Content-Type" header
 		});
-		console.log(response);
+
+		
+		let respData = await response.json();
+			console.log(respData);
 	}
 	// console.log(response);
-
-
 	return (
 		<>
 			<h2>PedidosScreen</h2>
@@ -61,8 +61,12 @@ export const PedidosScreen = () => {
 						value={registrarPedidoInput}
 						onChange={handleInputChange}
 						autoComplete={"off"}
+						autoFocus={true}
 					></input>
 				</form>
+				
+			</div>
+			<div>
 			</div>
 		</>
 	);
